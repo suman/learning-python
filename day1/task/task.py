515283aceb38a6245a2586eb307f7c16bac8264d
Suman Bogati <sumanbogati@gmail.com>
2025-06-27 18:26:38 +0545

    Exercise: input, variable

---
+++ day1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello user! good morning!");
+city = input("Which city you grew up")
+pet_name = input("What is your pet name")
+print("Great! you grew up in " + city + " " + "and your pet name is " + pet_name + "!")
